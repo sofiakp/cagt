f381b32b6559d4be54286350d47c849bc29dcd4b
Deuscide@gmail.com <Deuscide@gmail.com@6b60b4a6-d3ce-c90b-0dfa-4a94c094ccc1>
2011-02-13 21:23:10 +0000

    fixing naming bug in ProfileData.py

--- trunk/src/ProfileData/ProfileData.py
+++ trunk/src/ProfileData/ProfileData.py
@@ -7,14 +7,15 @@ from src.analysis.get_low_signal_profiles import get_low_signal_profiles
 from src.utils import *
 from src.file_processing.read_profiles_file import read_profiles_file
 from src.output.rpy_matrix_conversion import np_matrix_to_r
+from src.ProfilesInfo.ProfilesInfo import *
 
 def append_data(data1, data2):
 	assert(len(data1.ids) == len(data2.ids))
 	data = MatrixMap(sp.hstack((data1.data, data2.data)))
 	return data
 
-def make_ProfileData(profiles_info, clustering_info):
-	if isinstance(profiles_info, ProfileInfoPair):
+def make_ProfileData(profile_info, clustering_info):
+	if isinstance(profile_info, ProfileInfoPair):
 		data1, peaks1 = read_profiles_file(profile_info.profiles_info1.filename)
 		data2, peaks2 = read_profiles_file(profile_info.profiles_info2.filename)
 		assert(len(peaks1) == len(peaks2))
@@ -24,15 +25,15 @@ def make_ProfileData(profiles_info, clustering_info):
 		# and the left for the left, but that's too much work, and it's not clear
 		# how it would work given that we use the 90%th percentile.  So we'll
 		# use the min of the two for both sides.
-		low_signal_cutoff_value = min(profiles_info.profiles_info1.low_signal_cutoff_value,\
+		low_signal_cutoff_value = min(profile_info.profiles_info1.low_signal_cutoff_value,\
 		profiles_info.profiles_info2.low_signal_cutoff_value)
 	else:
 		data, peaks = read_profiles_file(profile_info.filename)
-		low_signal_cutoff_value = profiles_info.low_signal_cutoff_value
+		low_signal_cutoff_value = profile_info.low_signal_cutoff_value
 	num_peaks = len(peaks)
 	low_signal_ids = get_low_signal_profiles(data, low_signal_cutoff_value)
 	high_signal_ids = opposite_ids(low_signal_ids, data.ids)
-	PD = ProfileData(profiles_info, clustering_info, data, low_signal_ids, high_signal_ids)
+	PD = ProfileData(profile_info, clustering_info, data, low_signal_ids, high_signal_ids)
 	return PD, peaks, num_peaks, low_signal_ids, high_signal_ids
 
 class ProfileData:
